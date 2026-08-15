c266e527b58b036e47b37f7b61e2ec8d9162d2e1
minoo m.amini <minoomamini@gmail.com>
2026-08-15 19:09:40 +0330

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,86 @@
-import Image from "next/image";
-
-export default function Home() {
+function page() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      {/*headear*/}
+      <div className=" flex justify-center" dir="rtl">
+        <ul className="flex">
+          <li className="mt-3">
+            <h1 className="text-lime-500 text-5xl">!Snap</h1>
+          </li>
+          <li className="relative group mt-5">
+            <span className=" text-xl m-3 cursor-pointer "> سوپراپ اسنپ </span>
+
+            <ul className="absolute right-0 top-full hidden w-40 group-hover:block">
+              <li className="w-full px-4 py-2  hover:bg-gray-300">
+                <span className=" text-xl m-3 cursor-pointer">
+                  {" "}
+                  درخواست تاکسی
+                </span>
+              </li>
+              <li className="w-full px-4 py-2 hover:bg-gray-300">
+                <span className="text-xl  cursor-pointer"> پیک موتوری</span>
+              </li>
+              <li className="w-full px-4 py-2  hover:bg-gray-300">
+                <span className="text-xl cursor-pointer ">
+                  {" "}
+                  سفارش آنلاین غذا اسنپ فود
+                </span>
+              </li>
+              <li className="w-full px-4 py-2  hover:bg-gray-300">
+                <span className="text-xl  cursor-pointer">اسنپ بیمه </span>
+              </li>
+              <li className="w-full px-4 py-2 hover:bg-gray-300">
+                <span className="text-xl cursor-pointer">
+                  {" "}
+                  اسنپ پرواز (بلیط هواپیما اسنپ)
+                </span>
+              </li>
+              <li className="w-full px-4 py-2 hover:bg-gray-300">
+                <span className="text-xl cursor-pointer">
+                  سوپرمارکت آنلاین اسنپ مارکت
+                </span>
+              </li>
+            </ul>
+          </li>
+          <li className="mt-5">
+            <span className=" text-xl m-3 cursor-pointer">
+              {" "}
+              ثبت نام راننده اسنپ
+            </span>
+          </li>
+          <li className="mt-5">
+            <span className="text-xl m-3 cursor-pointer">
+              {" "}
+              اسنپ پی (سرویس اعتباری)
+            </span>
+          </li>
+          <li className="mt-5">
+            <span className="text-xl m-3 cursor-pointer"> پنل سازمانی</span>
+          </li>
+          <li className="mt-5">
+            <span className="text-xl m-3 cursor-pointer"> باشگاه رانندگان</span>
+          </li>
+          <li className="mt-5">
+            <span className="text-xl m-3 cursor-pointer"> بلاگ</span>
+          </li>
+          <li className="relative group mt-5">
+            <span className="text-xl m-3 cursor-pointer"> درباره اسنپ</span>
+            <ul className="absolute right-0 top-full hidden w-40 group-hover:block">
+              <li className="w-full px-4 py-2  hover:bg-gray-300">
+                <span className="text-xl cursor-pointer "> فرصت‌های شغلی</span>
+              </li>
+              <li className="w-full px-4 py-2  hover:bg-gray-300">
+                <span className="text-xl cursor-pointer "> درباره ما</span>
+              </li>
+              <li className="w-full px-4 py-2  hover:bg-gray-300">
+                <span className="text-xl cursor-pointer "> تماس با ما</span>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
+
+export default page;
