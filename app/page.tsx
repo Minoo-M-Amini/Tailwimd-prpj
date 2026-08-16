d74a9de24f899e45ae3f3a81e83a5bcf49d90864
minoo m.amini <minoomamini@gmail.com>
2026-08-17 02:11:36 +0330

    saling & mini changes

--- app/page.tsx
+++ app/page.tsx
@@ -353,22 +353,282 @@ function page() {
         </div>
       </>
 
-      {/*saling*/}
+      {/*saling-1*/}
+      <>
+        <div className="flex justify-between m-auto container mb-3" dir="rtl">
+          <p className="text-2xl">خرید به صرفه ی بیمه</p>
+          <span className="flex cursor-pointer">
+            <img
+              src="icons8-right-arrow-30.png"
+              alt="icons8-right-arrow-30"
+              className="h-3 w-3 ml-1 "
+            />
+            <img
+              src="icons8-left-arrow-30.png"
+              alt="icons8-left-arrow-30"
+              className="h-3 w-3"
+            />
+          </span>
+        </div>
+
+        <div className="container m-auto flex justify-between" dir="rtl">
+          <br />
+
+          <div className="flex justify-between shadow-lg">
+            <a href="" className="ml-10">
+              <img
+                src="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-14-0.jpg"
+                alt="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-14-0"
+                className="rounded"
+              />
+              <div className="flex">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl m-auto"> بیمه ثالث خودرو </p>
+                <button className=" m-auto  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+          <div className="flex justify-between shadow-lg">
+            <a href="" className="ml-10">
+              <img
+                src="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-14-1.jpg"
+                alt="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-14-1"
+                className="rounded"
+              />
+              <div className="flex ">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl mt-2.5 mr-2 "> بیمه ثالث خودرو </p>
+                <button className="mr-45 mt-2  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+          <div className="flex justify-between shadow-lg">
+            <a href="">
+              <img
+                src="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-14-2.jpg"
+                alt="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-14-2"
+                className="rounded"
+              />
+              <div className="flex">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl mt-2.5 mr-2 "> بیمه ثالث خودرو </p>
+                <button className="mr-45 mt-2  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+        </div>
+      </>
       <br />
       <br />
       <br />
       <br />
+      {/*saling-2*/}
+      <>
+        <div className="flex justify-between m-auto container mb-3" dir="rtl">
+          <p className="text-2xl">خرید به صرفه ی بیمه</p>
+          <span className="flex cursor-pointer">
+            <img
+              src="icons8-right-arrow-30.png"
+              alt="icons8-right-arrow-30"
+              className="h-3 w-3 ml-1"
+            />
+            <img
+              src="icons8-left-arrow-30.png"
+              alt="icons8-left-arrow-30"
+              className="h-3 w-3"
+            />
+          </span>
+        </div>
+
+        <div className="container m-auto flex justify-between pb-2" dir="rtl">
+          <br />
+
+          <div className="flex justify-between shadow-lg">
+            <a href="" className="ml-10">
+              <img
+                src="		https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-17-0.jpg"
+                alt="		https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-17-0"
+                className="rounded"
+              />
+              <div className="flex">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl m-auto"> بیمه ثالث خودرو </p>
+                <button className=" m-auto  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+          <div className="flex justify-between shadow-lg">
+            <a href="" className="ml-10">
+              <img
+                src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-17-1.jpg"
+                alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-17-1"
+                className="rounded"
+              />
+              <div className="flex ">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl mt-2.5 mr-2 "> بیمه ثالث خودرو </p>
+                <button className="m-auto  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+          <div className="flex justify-between shadow-lg">
+            <a href="">
+              <img
+                src="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-17-2.jpg"
+                alt="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-17-2"
+                className="rounded"
+              />
+              <div className="flex">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl mt-2.5 mr-2 "> بیمه ثالث خودرو </p>
+                <button className="m-auto  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+        </div>
+      </>
       <br />
       <br />
       <br />
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      {/*saling-3*/}
+      <>
+        <div className="flex justify-between m-auto container mb-3" dir="rtl">
+          <p className="text-2xl">خرید به صرفه ی بیمه</p>
+          <span className="flex cursor-pointer">
+            <img
+              src="icons8-right-arrow-30.png"
+              alt="icons8-right-arrow-30"
+              className="h-3 w-3 ml-1"
+            />
+            <img
+              src="icons8-left-arrow-30.png"
+              alt="icons8-left-arrow-30"
+              className="h-3 w-3"
+            />
+          </span>
+        </div>
+
+        <div className="container m-auto flex justify-between" dir="rtl">
+          <br />
+
+          <div className="flex justify-between shadow-lg">
+            <a href="" className="ml-10">
+              <img
+                src="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-25-0.jpg"
+                alt="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-25-0"
+                className="rounded"
+              />
+              <div className="flex">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl m-auto"> بیمه ثالث خودرو </p>
+                <button className=" m-auto  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+          <div className="flex justify-between shadow-lg">
+            <a href="" className="ml-10">
+              <img
+                src="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-25-1.jpg"
+                alt="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-25-1"
+                className="rounded"
+              />
+              <div className="flex ">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl mt-2.5 mr-2 "> بیمه ثالث خودرو </p>
+                <button className="m-auto  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+          <div className="flex justify-between shadow-lg">
+            <a href="">
+              <img
+                src="	https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-25-2.jpg"
+                alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/promo-25-2"
+                className="rounded"
+              />
+              <div className="flex">
+                <img
+                  src="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance.png"
+                  alt="https://web-cdn.snapp.ir/snappir-marketing/images/home-promos/Insurance"
+                  className="w-10 h-10"
+                />
+                <p className="text-xl mt-2.5 mr-2 "> بیمه ثالث خودرو </p>
+                <button className="m-auto  border-solid border-2 border-blue-500 rounded">
+                  <a href="" className="text-xl m-3">
+                    خرید کنید
+                  </a>
+                </button>
+              </div>
+            </a>
+          </div>
+        </div>
+      </>
+
       <br />
       <br />
       <br />
@@ -528,6 +788,11 @@ function page() {
           </a>
         </div>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 }
